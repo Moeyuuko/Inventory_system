59bf696dfe761e97256c13df448b55e4e3b707e2
Moeyuuko <zihao890@gmail.com>
2021-09-29 03:20:21 +0800

    初提交

---
+++ Read.php
@@ -0,0 +1,83 @@
+<?php
+    foreach($_GET as $key=>$val) {            // read get request from web
+        $SN = $val;
+    }
+//$servername = "";                //connect db
+//$username = "";
+//$password = "*";
+//$dbname = "";
+include(".key.php");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM `device` WHERE `SN` = '$SN'";
+// echo ($sql);
+mysqli_query($conn, 'set names utf8');
+if(!mysqli_query($conn, $sql))
+{
+    die('Error : ' . mysqli_error($conn));
+}
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output db
+    while($row = $result->fetch_assoc()) {
+$TAG = $row["TAG"];
+$TIME = $row["TIME"];
+$NOTE = $row["NOTE"];
+$N1 = $row["N1"];
+$N2= $row["N2"];
+header('Content-Type:text/html; charset=UTF-8');
+echo"<body>";
+echo"<div style='text-align: center'>";
+echo"<h1>";
+print ("Wellcome to FKUN QR Service");
+echo"</h1>";
+echo"</br>";
+echo"<div style='border: 5px solid;'>";
+echo"<p style='font-size: 5vw; text-align: left; margin-left: 10%; width:80%;'>";
+print ("SN：".$SN);
+echo"</br>";
+print ("TAG：".$TAG);
+echo"</br>";
+if($TIME != NULL){
+print ("Date：".$TIME);
+}else{
+print ("Date：Unknow");
+}
+
+if($NOTE != NULL){
+echo"</br>";
+print ("INFO：");
+echo"<p style='font-size: 4vw; text-align: left; margin-left: 10%; width:80%; height: 30%; border: 5px inset;'>";
+print ($NOTE);
+echo"</p>";
+}else{
+}
+if($N1 != NULL){
+echo"</br>";
+echo"<p style='font-size: 3vw; text-align: left; margin-left: 10%; width:80%; border: 5px inset;'>";
+print ($N1);
+echo"</p>";
+}else{
+}
+if($N2 != NULL){
+echo"</br>";
+echo"<p style='font-size: 3vw; text-align: left; margin-left: 10%; width:80%; border: 5px inset;'>";
+print ($N2);
+echo"</p>";
+}else{
+}
+echo"</p>";
+echo"</div>";
+echo"</div>";
+echo"</body>";
+}
+} else {
+    echo "Not Found";
+}
+$conn->close();
+?>
---
+++ sql/device.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主机： localhost
+-- 生成日期： 2021-09-29 01:37:45
+-- 服务器版本： 5.7.34
+-- PHP 版本： 7.3.27-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库： `Inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `device`
+--
+
+CREATE TABLE `device` (
+  `ID` int(11) NOT NULL,
+  `SN` varchar(15) NOT NULL,
+  `TAG` mediumtext,
+  `TIME` varchar(15) DEFAULT NULL,
+  `NOTE` mediumtext,
+  `N1` mediumtext,
+  `N2` mediumint(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `device`
+--
+ALTER TABLE `device`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
