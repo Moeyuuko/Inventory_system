55cb8efc4c2077a5d22eef9dbf8901f610422a05
Moeyuuko <zihao890@gmail.com>
2021-09-30 01:33:00 +0800

    ADD UNIQUE KEY SN

--- sql/device.sql
+++ sql/device.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2021-09-29 01:37:45
+-- 生成日期： 2021-09-30 01:04:54
 -- 服务器版本： 5.7.34
 -- PHP 版本： 7.3.27-1~deb10u1
 
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- 数据库： `Inventory`
@@ -34,8 +34,15 @@ CREATE TABLE `device` (
   `TIME` varchar(15) DEFAULT NULL,
   `NOTE` mediumtext,
   `N1` mediumtext,
-  `N2` mediumint(9) DEFAULT NULL
+  `N2` mediumtext
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `device`
+--
+
+INSERT INTO `device` (`ID`, `SN`, `TAG`, `TIME`, `NOTE`, `N1`, `N2`) VALUES
+(0, 'm1cQ1hI6', 'TEST_01', '2021/9/29', '测试物品', '<img width=100% src=\"https://moe-gz-public.oss-cn-guangzhou.aliyuncs.com/I_sys/%E4%BD%A0%E5%A5%BD.jpg\">', NULL);
 
 --
 -- 转储表的索引
@@ -45,7 +52,8 @@ CREATE TABLE `device` (
 -- 表的索引 `device`
 --
 ALTER TABLE `device`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `SN` (`SN`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -55,7 +63,7 @@ ALTER TABLE `device`
 -- 使用表AUTO_INCREMENT `device`
 --
 ALTER TABLE `device`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
