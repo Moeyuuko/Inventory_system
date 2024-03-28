9106da0392eb59c13a13c0066d347044db694557
Moeyuuko <zihao890@gmail.com>
2024-03-28 14:30:40 +0800

    fix: 初始化json数据 & 修正变量名

--- Read.php
+++ Read.php
@@ -13,6 +13,8 @@ if ($conn->connect_error) {
 mysqli_query($conn, 'set names utf8');
 
 $SN = $_GET['key'];
+// include("module/SendMessage/SendMessage.php");
+// Smg_Check($SN);
 if(strlen($SN)<=12 && strlen($SN)>0){
 	$SN = mysqli_real_escape_string($conn,$SN);
 	$sql = "SELECT * FROM `device` WHERE `SN` = '$SN'";
@@ -20,59 +22,59 @@ if(strlen($SN)<=12 && strlen($SN)>0){
 	if(!mysqli_query($conn, $sql)){die('Error : ' . mysqli_error($conn));}
 	$result = $conn->query($sql);
 	$conn->close();
-
+	$json_data = new stdClass(); //初始化json数据
 	if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-				$Security = $row["Security"]; //安全等级 0公开 1私密 2半公开隐藏NOTE,N1,N2
-				$json_deta->Security = $Security;				
-				switch ($Security){
-					case 0:
-						$json_deta->SN = $SN = $row["SN"];
-						$json_deta->TAG = $TAG = $row["TAG"];
-						$json_deta->TIME = $TIME = $row["TIME"];
-						$json_deta->NOTE = $NOTE = $row["NOTE"];
-						$json_deta->N1 = $N1 = $row["N1"];
-						$json_deta->N2 = $N2= $row["N2"];
-						break;
-					case 1:
-						if (empty ( $_SESSION ['user'] )){
-							if($_GET['type']=='json'){  #json输出
-								header('content-type:application/json;charset=utf8');
-								echo json_encode($json_deta, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-							}else{
-								header ( "location:login.php?req_url=" . $_SERVER ['REQUEST_URI'] );
-							}
-							exit();
+		while($row = $result->fetch_assoc()) {
+			$Security = $row["Security"]; //安全等级 0公开 1私密 2半公开隐藏NOTE,N1,N2
+			$json_data->Security = $Security;				
+			switch ($Security){
+				case 0:
+					$json_data->SN = $SN = $row["SN"];
+					$json_data->TAG = $TAG = $row["TAG"];
+					$json_data->TIME = $TIME = $row["TIME"];
+					$json_data->NOTE = $NOTE = $row["NOTE"];
+					$json_data->N1 = $N1 = $row["N1"];
+					$json_data->N2 = $N2= $row["N2"];
+					break;
+				case 1:
+					if (empty ( $_SESSION ['user'] )){
+						if($_GET['type']=='json'){  #json输出
+							header('content-type:application/json;charset=utf8');
+							echo json_encode($json_data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+						}else{
+							header ( "location:login.php?req_url=" . $_SERVER ['REQUEST_URI'] );
 						}
-						$json_deta->SN = $SN = $row["SN"];
-						$json_deta->TAG = $TAG = $row["TAG"];
-						$json_deta->TIME = $TIME = $row["TIME"];
-						$json_deta->NOTE = $NOTE = $row["NOTE"];
-						$json_deta->N1 = $N1 = $row["N1"];
-						$json_deta->N2 = $N2= $row["N2"];
-						break;
-					case 2:
-						$json_deta->SN = $SN = $row["SN"];
-						$json_deta->TAG = $TAG = $row["TAG"];
-						$json_deta->TIME = $TIME = $row["TIME"];
-						$json_deta->N2 = $N2= $row["N2"]; //N2属于半公开
-						if (!empty ( $_SESSION ['user'] )){
-							$json_deta->NOTE = $NOTE = $row["NOTE"];
-							$json_deta->N1 = $N1 = $row["N1"];
-						}
-						break;
-					default:
-						$SN = "安全级别错误";
-						$TAG = "安全级别错误";
-						$TIME = "安全级别错误";
-				}
-				if($_GET['type']=='json'){  #json输出
-					header('content-type:application/json;charset=utf8');
-					echo json_encode($json_deta, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-				}else{
-					include ('src/Read.html.php');
-				}
+						exit();
+					}
+					$json_data->SN = $SN = $row["SN"];
+					$json_data->TAG = $TAG = $row["TAG"];
+					$json_data->TIME = $TIME = $row["TIME"];
+					$json_data->NOTE = $NOTE = $row["NOTE"];
+					$json_data->N1 = $N1 = $row["N1"];
+					$json_data->N2 = $N2= $row["N2"];
+					break;
+				case 2:
+					$json_data->SN = $SN = $row["SN"];
+					$json_data->TAG = $TAG = $row["TAG"];
+					$json_data->TIME = $TIME = $row["TIME"];
+					$json_data->N2 = $N2= $row["N2"]; //N2属于半公开
+					if (!empty ( $_SESSION ['user'] )){
+						$json_data->NOTE = $NOTE = $row["NOTE"];
+						$json_data->N1 = $N1 = $row["N1"];
+					}
+					break;
+				default:
+					$SN = "安全级别错误";
+					$TAG = "安全级别错误";
+					$TIME = "安全级别错误";
 			}
+			if($_GET['type']=='json'){  #json输出
+				header('content-type:application/json;charset=utf8');
+				echo json_encode($json_data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+			}else{
+				include ('src/Read.html.php');
+			}
+		}
 	} else {
 		if($_GET['type']=='json'){  #json输出
 			http_response_code(404); #json方式返回404表示找不到
